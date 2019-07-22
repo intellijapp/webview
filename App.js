6466a436206ceafced0d3e09ebe12e2fd69c5078
bhlshrf <bhlshrf@gmail.com>
2019-07-22 16:19:14 +0300

    improve

--- App.js
+++ App.js
@@ -7,15 +7,15 @@ const website = 'developer.hesbaty.com';
 
 export default class App extends Component {
   state = {
-    loading: true,
     error: false,
     isConnected: true,
     isBackOnline: false,
+    showSplashScreen: true,
   }
 
   unsubscribe = null;
   componentDidMount() {
-    BackHandler.addEventListener('hardwareBackPress', this.handleBackButton.bind(this));
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
     this.unsubscribe = NetInfo.addEventListener(state => {
       let isBackOnline = !this.state.isConnected && state.isConnected && state.isInternetReachable;
       this.setState({ isBackOnline: isBackOnline, isConnected: state.isConnected && state.isInternetReachable, });
@@ -25,7 +25,7 @@ export default class App extends Component {
     StatusBar.setHidden(true);
   }
   componentWillUnmount() {
-    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton.bind(this));
+    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
     if (this.unsubscribe)
       this.unsubscribe();
   }
@@ -37,83 +37,66 @@ export default class App extends Component {
   }
 
   inHomePage = () => {
+    if (this.currentUrl)
+      return true;
     let url = this.currentUrl.endsWith('/') ? this.currentUrl.substr(0, url.left - 1) : this.currentUrl;
     return url === 'http://' + website || url == 'https://' + website;
   }
 
   onShouldStartLoadWithRequest = (navigator) => {
     this.currentUrl = navigator.url.toLowerCase();
-    if (this.currentUrl.includes('://' + website) !== true) {
+    if (this.wv && this.currentUrl && this.currentUrl.includes('://' + website) !== true) {
       this.wv.stopLoading();
       return false;
     }
     return true;
   }
 
-  handelError = () => {
-    this.wv.reload();
-    this.setState({ loading: true, error: false })
-  }
-
   render() {
-    return <View style={{ flex: 1, }}>
-      <View style={{ height: 60, width: '100%', padding: 10, backgroundColor: '#33a' }}>
-        <View style={{ flex: 4, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-          <Image source={require('./assets/icon.png')} resizeMode='cover'
-            style={{ height: 50, width: 50, marginHorizontal: 10 }}
-          />
-          <Text style={{ color: '#fff', fontSize: 21 }}>Hesbaty</Text>
-        </View>
-      </View>
-      {
-        !this.state.isConnected && <View style={{
-          width: '100%',
-          backgroundColor: '#f00', padding: 6,
-          alignItems: 'center'
-        }}>
-          <Text style={{ color: '#fff', }}>No Connection</Text>
-        </View>
-      }
-
-      {
-        this.state.isBackOnline && <View style={{
-          width: '100%',
-          backgroundColor: '#0b0', padding: 6,
-          alignItems: 'center'
-        }}>
-          <Text style={{ color: '#fff', }}>You online now</Text>
-        </View>
-      }
+    return <View style={{ flex: 1, backgroundColor: '#fff' }}>
+      <Navbar />
+      {!this.state.isConnected && <Notify color='#f00' text='No Connection' />}
+      {this.state.isBackOnline && <Notify color='#0b0' text='You online now' />}
+      {this.state.error && <Error onRety={this.wv.reload} />}
 
       <WebView
         source={{ uri: 'http://' + website }}
         ref={wv => this.wv = wv}
         onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest} //for iOS
         onNavigationStateChange={this.onShouldStartLoadWithRequest} //for Android
-        onLoad={() => this.setState({ loading: false, error: false })}
-        onError={() => this.setState({ loading: false, error: true })}
+        onError={() => this.setState({ error: true })}
+        onLoadStart={() => this.setState({ error: false })}
+        onLoadEnd={() => this.setState({ showSplashScreen: false })}
+        renderLoading={this.loader}
+        javaScriptEnabled
+        domStorageEnabled
+        startInLoadingState
       />
-      {this.renderLoader()}
-      {this.renderError()}
     </View>;
   }
 
-  renderLoader = () => !this.state.loading ? null : <View style={{
-    position: 'absolute', top: 0, left: 0, bottom: 0, right: 0,
-    flex: 1, justifyContent: 'center', alignItems: 'center',
-    backgroundColor: 'rgba(255,255,255,0.8)',
-  }}>
-    <Image source={require('./assets/icon.png')}
-      style={{ width: 200, height: 200 }} />
-    <ActivityIndicator size="large" />
-  </View>;
-
-  renderError = () => !this.state.error ? null : <View style={{
-    position: 'absolute', top: 0, left: 0, bottom: 0, right: 0,
-    flex: 1, justifyContent: 'center', alignItems: 'center',
-    backgroundColor: '#fff'
-  }}>
-    <Text style={{ padding: 16, fontWeight: 'bold' }}>Network Error</Text>
-    <Button onPress={this.handelError} title='Retry' />
-  </View>;
+  loader = () => !this.state.showSplashScreen ? <ActivityIndicator size='large' style={{ marginVertical: 100 }} /> :
+    <View style={{ height: '80%', justifyContent: 'center', alignItems: 'center', backgroundColor: '#fffb', }}>
+      <Image source={require('./assets/icon.png')} style={{ width: 200, height: 200 }} />
+      <ActivityIndicator size="large" />
+    </View>;
 }
+
+const Navbar = () => <View style={{ height: 60, width: '100%', padding: 10, backgroundColor: '#33a' }}>
+  <View style={{ flex: 4, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+    <Image source={require('./assets/icon.png')} resizeMode='cover'
+      style={{ height: 50, width: 50, marginHorizontal: 10 }}
+    />
+    <Text style={{ color: '#fff', fontSize: 21 }}>Hesbaty</Text>
+  </View>
+</View>;
+
+const Notify = ({ color, text, textColor = '#fff' }) => <View
+  style={{ width: '100%', backgroundColor: color, padding: 6, alignItems: 'center' }}>
+  <Text style={{ color: textColor, }}>{text}</Text>
+</View>;
+
+const Error = ({ onRety }) => <View style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: '#fffb' }}>
+  <Text style={{ padding: 16, fontWeight: 'bold' }}>Network Error</Text>
+  <Button onPress={onRety} title='Retry' />
+</View>;
